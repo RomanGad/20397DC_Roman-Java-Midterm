e7936268ae6574114a7926491e6a43b0ddd49d2f
RomanGad <romanergogo@gmail.com>
2022-04-06 22:38:28 -0400

    Complete lowestNumber class from math_problem package

--- src/math_problems/FindMissingNumber.java
+++ src/math_problems/FindMissingNumber.java
@@ -1,13 +1,14 @@
 package math_problems;
 
-public class FindMissingNumber {
+import java.util.ArrayList;
+import java.util.List;
+
 
     /** INSTRUCTIONS
      * Write a method to find the missing number from the array.
      */
 
-    public static void main(String[] args) {
-        int[] array = new int[] {10, 2, 1, 4, 5, 3, 7, 8, 6};
 
-    }
-}
+
+
+
--- src/math_problems/LowestNumber.java
+++ src/math_problems/LowestNumber.java
@@ -10,9 +10,22 @@ public class LowestNumber {
 
        int[] array = new int[] {211, 110, 99, 34, 67, 89, 67, 456, 321, 456, 78, 90, 45, 32, 56, 78, 90, 54, 32, 123,
                67, 5, 679, 54, 32, 65};
-   {
-                    System.out.println();
-        }
+       int []low =array;
+       int temp, size;
+
+       size = array.length;
+       for(int i= 0; i<size; i++){
+           for (int n=i+1; n<size; n++){
+               if(low[i]>low[n]){
+                   temp=low[i];
+                   low[i]= low[n];
+                   low[n]= temp;
+               }
+           }
+       }
+       System.out.println("The lowest number of this array list is: " + low[0]);
+
+
 
 
 
