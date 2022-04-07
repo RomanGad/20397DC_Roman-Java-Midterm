db18ef24dc864a4810708be425ca56e58381542b
RomanGad <romanergogo@gmail.com>
2022-04-07 16:39:25 -0400

    complete findMissingNumber class of math_problem

--- src/math_problems/FindMissingNumber.java
+++ src/math_problems/FindMissingNumber.java
@@ -3,11 +3,32 @@ package math_problems;
 import java.util.ArrayList;
 import java.util.List;
 
-
+public class FindMissingNumber {
     /** INSTRUCTIONS
      * Write a method to find the missing number from the array.
      */
 
+    public static int missingNumber(int[]numbers)
+    {
+        int n= numbers.length;
+        int total = ((n+1)*(n+2))/2;
+
+        for(int i=0; i< n; i++)
+            total-=numbers[i];
+        return total;
+    }
+public static void main(String[] args) {
+
+    int[] array = new int[]{10, 2, 1, 4, 5, 3, 7, 8, 6};
+
+    int [] mis = array;
+
+    System.out.println(missingNumber((mis)));
+
+
+
+        }
+        }
 
 
 
