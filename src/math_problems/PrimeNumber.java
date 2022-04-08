856cd7c16965b0c2e14823801a67728c9cee07b4
RomanGad <romanergogo@gmail.com>
2022-04-08 00:57:52 -0400

    make a change on primeNumber class of mathPackage

--- src/math_problems/PrimeNumber.java
+++ src/math_problems/PrimeNumber.java
@@ -11,18 +11,22 @@ public class PrimeNumber {
 
 
     public static void main(String[] args) {
-        for (int num=0; num<=100; num+=2){
-            if(isPrimeNumber(num));{
-                System.out.println(num);
+
+        for (int num = 2; num < 100000; num++){
+            boolean isPrimeNumber = true;
+            for (int i = 2; i < num; i++) {
+                if (num % i == 0) ;{
+                    isPrimeNumber = false;
+                    break;
+
+                }
+            }
+            if (isPrimeNumber==true){
+                System.out.println(num + " ");
             }
         }
     }
-    private static boolean isPrimeNumber(int num) {
-        for (int i = 1; i >num; i--)
-            if (num%i == 0) {
-                return false;
 
-            }
-        return true;
-    }
-    }
+
+}
+
