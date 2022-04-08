b4661edaf36014b81b66fba00197b4ed9279f8f9
RomanGad <romanergogo@gmail.com>
2022-04-08 11:53:00 -0400

    complete Anagram class from string_problems package

--- src/string_problems/Anagram.java
+++ src/string_problems/Anagram.java
@@ -1,5 +1,7 @@
 package string_problems;
 
+import java.util.Arrays;
+
 public class Anagram {
 
     /*
@@ -13,6 +15,26 @@ public class Anagram {
             "FART" & "RAFT"
     */
 
-    //Implement Here
+    public static void main(String[] args) {
+        String name1 ="earth";
+        String name2 ="heart";
+
+        if(name1.length() ==name2.length()){
+
+            char [] charArray1 = name1.toCharArray();
+            char [] charArray2 = name2.toCharArray();
+
+            Arrays.sort(charArray1);
+            Arrays.sort(charArray2);
+
+            boolean isAnagram = Arrays.equals(charArray1, charArray2);
+            if(isAnagram) {
+                System.out.println(name1 +" " + "and" + " " + name2 + " " + "are anagrams. ");
+            } else {
+                System.out.println(name1 + " " + "and" + " " + name2 + " " + "are not anagrams. ");
+
+            }
+        }
+    }
 
 }
