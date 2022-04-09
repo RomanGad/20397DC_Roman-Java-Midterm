fdb0fbc4af123e8b6d0db4c3ea59f1210a63a49f
RomanGad <romanergogo@gmail.com>
2022-04-09 15:15:38 -0400

    make a change on pattern class from math_problem package

--- src/math_problems/Pattern.java
+++ src/math_problems/Pattern.java
@@ -3,7 +3,7 @@ package math_problems;
 //import com.mongodb.client.model.geojson.CoordinateReferenceSystem;
 //import com.mysql.cj.x.protobuf.MysqlxDatatypes;
 
-public class Pattern {
+public class Pattern<value> {
 
 
 
@@ -14,17 +14,28 @@ public class Pattern {
      * 100,99,98,97,96,95,94,93,92,91,90,88,86,84,82,80,78,76,74,72,70,67,64,61,58,55,52,49,46,43,40,36,32...
      *
      */
-     static void printReverseOrder(int D)
-{
-    for (int i = D; i>0; i--)
-        System.out.print(i + " ");
-}
-    public static void main(String[] args) {
-        int D = 100;
-        printReverseOrder(D);
-    }
+ //Range 100 to 90 (+1)
+ //Range 90 to 70 (+2)
+ //Range 70 to 40 (+3)
+ //Range 40 to 0 (+4)
 
-}
+    public static void main(String[] args) {
+        int value = 100;
+        int firstNumber = 1;
+        int secondNumber =1;
+
+        while (value>=0){
+            System.out.print(value + " ");
+            value-= secondNumber;
+            firstNumber++;
+
+            if (firstNumber == 11){
+                firstNumber=1;
+                secondNumber++;
+            }
+        }
+    }
+    }
 
 
 
